700f96552572f37fdb75aad1bc5d362369cddd51
fujiwara takeshi <tfdxvjiyre75528@gmail.com>
2024-04-01 17:21:28 +0900

    テスト用環境

--- src/resources/views/user.blade.php
+++ src/resources/views/user.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('css')
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+<link rel="stylesheet" href="{{ asset('css/bootstrap-4.css') }}">
 <link rel="stylesheet" href="{{ asset('css/user.css') }}">
 @endsection
 
